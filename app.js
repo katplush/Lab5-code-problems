8c898d1c171dc59d31d2fe6e0889830ff805a5d6
Katerina Plushko <katplush@yahoo.com>
2018-01-20 17:36:31 -0800

    problem 4 test passed

--- app.js
+++ app.js
@@ -80,15 +80,24 @@ IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
+
 var testArray = [2, 3, 4]; //eslint-disable-line
 
+
 function sumArray(testArray) {
+  // var theSum = sum(testArray[0], testArray[1], testArray[2][0][0]);
+  // var arrayStr = 'testArray +  ' was passed in as an array of numbers, and ' + theSum + ' is their sum.';
+  var total = 0;
+  for (var i = 0; i < testArray.length; i++) {
+    total = sum(total, testArray[i])[0];
+  }
+  var arrayStr= (testArray + ' was passed in as an array of numbers, and ' + total + ' is their sum.');
+  return [total, arrayStr];
   //eslint-disable-line
 }
-
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
